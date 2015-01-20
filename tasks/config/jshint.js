ae36b601a85003f4c0dc1731cc1cdb4ecc3127a3
Andrew Sednev <exantrius@gmail.com>
2015-01-20 11:55:40 +0300

    add lint and jscs

---
+++ tasks/config/jshint.js
@@ -0,0 +1,47 @@
+/**
+ * @file: /tasks/config/js_hint.js
+ * @author Andrew Sednev <exantrius@gmail.com>
+ */
+module.exports = function (grunt) {
+    grunt.config.set('jshint', {
+        options: {
+            "predef": [
+                "document",
+                "window",
+            ],
+            "globals": {
+                "module": true
+            },
+            "curly":     false,
+            "eqeqeq":    true,
+            "funcscope": true,
+            "browser":   true,
+            "immed":     false,
+            "strict":    false,
+            "newcap":    true,
+            "noempty":   false,
+            "nonew":     true,
+            "undef":     true,
+            "unused":    true,
+            "boss":      true,
+            "eqnull":    true,
+            "sub":       true,
+            "devel":     true,
+            "forin":     false,
+            "noarg":     true,
+            "esnext":    true
+        },
+        api: {
+            files: {
+                src: ['api/**/*.js']
+            } 
+        },
+        assets: {
+            files: {
+                src: ['tasks/pipeline.js']
+            },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+}

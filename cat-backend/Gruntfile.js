e6ef3805e790009be024f69eb8f74021a49e1bdd
Andrew Sednev <exantrius@gmail.com>
2015-01-20 16:44:18 +0300

    move sails application to cat-backend

--- cat-backend/Gruntfile.js
+++ cat-backend/Gruntfile.js
@@ -12,8 +12,7 @@
  * Check out the `tasks` directory instead.
  */
 
-module.exports = function(grunt) {
-
+module.exports = function (grunt) {
 
     // Load the include-all library in order to require all of our grunt
     // configurations and task registrations dynamically.
@@ -24,7 +23,7 @@ module.exports = function(grunt) {
         try {
             includeAll = require('sails/node_modules/include-all');
         }
-        catch(e1) {
+        catch (e1) {
             console.error('Could not find `include-all` module.');
             console.error('Skipping grunt tasks...');
             console.error('To fix this, please run:');
@@ -36,14 +35,13 @@ module.exports = function(grunt) {
         }
     }
 
-
     /**
      * Loads Grunt configuration modules from the specified
      * relative path. These modules should export a function
      * that, when run, should either load/configure or register
      * a Grunt task.
      */
-    function loadTasks(relPath) {
+    function loadTasks (relPath) {
         return includeAll({
             dirname: require('path').resolve(__dirname, relPath),
             filter: /(.+)\.js$/
@@ -54,7 +52,7 @@ module.exports = function(grunt) {
      * Invokes the function from a Grunt configuration module with
      * a single argument - the `grunt` object.
      */
-    function invokeConfigFn(tasks) {
+    function invokeConfigFn (tasks) {
         for (var taskName in tasks) {
             if (tasks.hasOwnProperty(taskName)) {
                 tasks[taskName](grunt);
@@ -62,16 +60,15 @@ module.exports = function(grunt) {
         }
     }
 
-
-
-
     // Load task functions
-    var taskConfigurations = loadTasks('./tasks/config'),
-        registerDefinitions = loadTasks('./tasks/register');
+    var taskConfigurations = loadTasks('./tasks/config');
+    var registerDefinitions = loadTasks('./tasks/register');
 
     // (ensure that a default task exists)
     if (!registerDefinitions.default) {
-        registerDefinitions.default = function (grunt) { grunt.registerTask('default', []); };
+        registerDefinitions.default = function (grunt) {
+            grunt.registerTask('default', []);
+        };
     }
 
     // Run task functions to configure Grunt.
